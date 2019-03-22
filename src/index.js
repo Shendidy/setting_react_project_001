f3677809fd316311f34bc73a7f8f81d4b846c3bf
Shendidy <sherif@shendidy.com>
2019-03-22 01:08:48 +0000

    after step 10 in steps.txt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello Sherif!");
+import React from "react";
+import ReactDom from "react-dom"
+
+const Index = () => {
+  return <div>Hello Sherif!</div>
+};
+
+ReactDom.render(<Index />, document.getElementById('index'));
